28a5fdda4bc53997f0de10bdfd28532acc5ade7c
steph411 <etabalionel@gmail.com>
2023-02-17 22:04:49 +0100

    setup the API and UI inside the same folder

--- app/utils/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-import useProtectedRedirect from './useProtectedRedirect'
-import useClassesBasedOnScroll from './useClassesBasedOnScroll'
-
-
-
-export {
-  useClassesBasedOnScroll, 
-  useProtectedRedirect
-}
